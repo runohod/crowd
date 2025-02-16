0fd24bb3ba7dbbe0b5ed83579786ed5c842de943
runohod <andrei.rumynthev1728@yandex.ru>
2025-02-16 11:04:35 +0300

    +

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+import './App.css';
+import CatList from './CatLike';
 
-const CatList = () => {
-  const [cats, setCats] = useState([]);
-  const [likes, setLikes] = useState({});
-
-  useEffect(() => {
-    const fetchCats = async () => {
-      const response = await axios.get('https://api.thecatapi.com/v1/images/search?limit=10');
-      setCats(response.data);
-    };
-
-    fetchCats();
-  }, []);
-
-  const handleLike = (id) => {
-    setLikes((prevLikes) => ({
-      ...prevLikes,
-      [id]: (prevLikes[id] || 0) + 1,
-    }));
-  };
-
+function App() {
   return (
-    <div>
-      <h1>Котики</h1>
-      <div className="cat-list">
-        {cats.map((cat) => (
-          <div key={cat.id} className="cat-card">
-            <img src={cat.url} alt="Котик" />
-            <button onClick={() => handleLike(cat.id)}>Лайк</button>
-            <p>Лайков: {likes[cat.id] || 0}</p>
-          </div>
-        ))}
-      </div>
+    <div className="App">
+      <CatList />
     </div>
   );
-};
+}
 
-export default CatList;
+export default App;
