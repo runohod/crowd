d581565897e8d9b32a1914be9d5ed42f0dfbd020
runohod <andrei.rumynthev1728@yandex.ru>
2025-02-18 23:24:48 +0300

    +

--- src/App.jsx
+++ src/App.jsx
@@ -21,16 +21,62 @@ export default App;
 
 // говорим что функция асинхронная 
 async function getResponse() {
-  let response = await fetch('url')
+  let response = await fetch('https://jsonplaceholder.typicode.com/posts/1')
   
     //успешен ли запрос
     if (!response.ok) {
       throw new Error(`Ошибка HTTP: ${response.status}`);
     }
   
-  let content = await response.text()
+  let content = await response.json()
   console.log(content)
   
   }
   
   getResponse()
+
+
+
+  async function getResponse() {
+    try {
+      let response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+  
+      //проверка на положительный запрос
+      if (!response.ok) {
+        throw new Error(`Ошибка HTTP: ${response.status}`);
+      }
+  
+      //ответ как JSON
+      let content = await response.json();
+      console.log(content); // Выводим ответ в консоль
+    } catch (error) {
+      //сообщение об ошибке
+      console.error('Произошла ошибка:', error);
+    }
+  }
+  
+  // Вызываем функцию
+  getResponse();
+
+
+///////////ПРИМЕР 
+
+
+
+  let url = 'https://jsonplaceholder.typicode.com/users/1';
+  let response = await fetch(url);
+  let content = await responsse/json();
+
+  alert(content[0].author.login); 
+
+
+
+fetch('https://jsonplaceholder.typicode.com/users/1');
+.then(response => response.json())
+.then(data => {
+  console.log('Имя ${daa.name}');
+  console.log('Email $[data.email]');
+})
+
+.catch(error => console.error('ошибка ' error));
+
