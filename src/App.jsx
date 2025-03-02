b7208c9ec159ede84bc2b4b05a970c01fe67aca6
runohod <andrei.rumynthev1728@yandex.ru>
2025-03-02 13:38:37 +0300

    +

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCounter] = useState(0)
+  const [count, setCounter, setCat] = useState(0)
+  const [setCat] = useState(0)
   const handClick =() => {
     setCounter(count + 1);
   };
   
+  useEffect(()=>{
+    async function getResponse() {
+        try {
+          let response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+      
+          //проверка на положительный запрос
+          if (!response.ok) {
+            throw new Error(`Ошибка HTTP: ${response.status}`);
+          }
+      
+          //ответ как JSON
+          let content = await response.json();
+          console.log(content); // Выводим ответ в консоль
+          setCat(content); //сохраняетт котов в состояние
+        } catch (error) {
+          //сообщение об ошибке
+          console.error('Произошла ошибка:', error);
+        }
+      }
+      
+      // Вызываем функцию
+      getResponse();
+    
+    },[])
+
       return (
     <div className="App">
         <h3>кол-во кликов по котам:</h3>
@@ -14,33 +40,10 @@ function App() {
 
     </div>
   );
+
 }
 
-  useEffect(()=>{
-  async function getResponse() {
-      try {
-        let response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
-    
-        //проверка на положительный запрос
-        if (!response.ok) {
-          throw new Error(Ошибка HTTP: ${response.status});
-        }
-    
-        //ответ как JSON
-        let content = await response.json();
-        console.log(content); // Выводим ответ в консоль
-        setCat(data.result); //сохраняетт котов в состояние
-      } catch (error) {
-        //сообщение об ошибке
-        console.error('Произошла ошибка:', error);
-      }
-    }
-    
-    // Вызываем функцию
-    getResponse();
-  
-  },[])
-
+export default App; 
 
 ///////////ПРИМЕР 
 
