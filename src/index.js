77d6d7f8bbaad2b009b28b7c000461250e9d5b1e
runohod <andrei.rumynthev1728@yandex.ru>
2025-03-20 08:08:24 +0300

    +

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
-const obj = {
-  value: 42,
-  getValue: function () {
-    return this.value;
-  },
-};
+function groupBy(array, fn) {
+  let result = {}; // Создаем пустой объект для результата
 
-const unboundedGetValue = obj.getValue;
-console.log(unboundedGetValue()); // Что выведет?
+  array.forEach((item) => {
+      const key = fn(item); // Вычисляем ключ с помощью переданной функции
+      if (!result[key]) { // Если ключа еще нет в объекте, создаем пустой массив
+          result[key] = [];
+      }
+      result[key].push(item); // Добавляем элемент в соответствующий массив
+  });
+
+  return result; // Возвращаем результат
+}
+
+// Пример 1
+const array1 = [
+  { id: 1 },
+  { id: 1 },
+  { id: 2 }
+];
+
+const fn = (item) => item.id;
+
+console.log(groupBy(array1, fn));
+// {
+//   1: [{ id: 1 }, { id: 1 }],
+//   2: [{ id: 2 }]
+// }
