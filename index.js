a27e6b5181841906e73e85db14da2273566b52c2
runohod <andrei.rumynthev1728@yandex.ru>
2025-04-02 10:13:14 +0300

    +

--- index.js
+++ index.js
@@ -15,3 +15,24 @@ fetch("https://api.thecatapi.com/v1/images/search?page=0&limit=30", requestOptio
 
 
 
+function fetchImage() {
+  const apiKey = "Your-apiKey";
+  fetch("https://any-anime.p.rapidapi.com/anime/img", {
+    method: "GET",
+    headers: {
+      "x-rapidapi-key": apiKey,
+      "x-rapidapi-host": "any-anime.p.rapidapi.com",
+    },
+  })
+    .then((response) => response.blob())
+    .then((blob) => {
+      const imageUrl = URL.createObjectURL(blob);
+      const imageElement = document.createElement("img");
+      imageElement.src = imageUrl;
+      const container = document.getElementById("image-container");
+      container.appendChild(imageElement);
+    })
+    .catch((error) => console.error(error));
+}
+const button = document.getElementById("fetch-image-button");
+button.addEventListener("click", fetchImage);
