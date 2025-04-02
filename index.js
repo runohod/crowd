255a8a418045b8028077862238a9685a79a9ff68
runohod <andrei.rumynthev1728@yandex.ru>
2025-04-02 21:27:03 +0300

    +

--- index.js
+++ index.js
@@ -13,26 +13,41 @@ fetch("https://api.thecatapi.com/v1/images/search?page=0&limit=30", requestOptio
 
 // схранять котов у локал сторедж . А при удаление котво из избаранных кота нужно подчищать в локал сторадж
 
+const image = document.querySelector(".img");
+const url = "https://api.thecatapi.com/v1/images/search?page=0&limit=30";
 
+async function fetchHandler() {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
 
-function fetchImage() {
-  const apiKey = "Your-apiKey";
-  fetch("https://any-anime.p.rapidapi.com/anime/img", {
-    method: "GET",
-    headers: {
-      "x-rapidapi-key": apiKey,
-      "x-rapidapi-host": "any-anime.p.rapidapi.com",
-    },
-  })
-    .then((response) => response.blob())
-    .then((blob) => {
-      const imageUrl = URL.createObjectURL(blob);
-      const imageElement = document.createElement("img");
-      imageElement.src = imageUrl;
-      const container = document.getElementById("image-container");
-      container.appendChild(imageElement);
-    })
-    .catch((error) => console.error(error));
+    image.src = data.file;
+    console.log(response);
+  } catch (error) {
+    console.log(error)
+  }
 }
-const button = document.getElementById("fetch-image-button");
-button.addEventListener("click", fetchImage);
+
+fetchHandler()
+
+// function fetchImage() {
+//   const apiKey = "Your-apiKey";
+//   fetch("https://any-anime.p.rapidapi.com/anime/img", {
+//     method: "GET",
+//     headers: {
+//       "x-rapidapi-key": apiKey,
+//       "x-rapidapi-host": "any-anime.p.rapidapi.com",
+//     },
+//   })
+//     .then((response) => response.blob())
+//     .then((blob) => {
+//       const imageUrl = URL.createObjectURL(blob);
+//       const imageElement = document.createElement("img");
+//       imageElement.src = imageUrl;
+//       const container = document.getElementById("image-container");
+//       container.appendChild(imageElement);
+//     })
+//     .catch((error) => console.error(error));
+// }
+// const button = document.getElementById("fetch-image-button");
+// button.addEventListener("click", fetchImage);
